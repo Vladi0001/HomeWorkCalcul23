b9f4fbeaa815272717e0903b15c2a3ccdc9d7d9e
Vladi0404 <moskva.vladi@yandex.ru>
2023-08-08 10:37:31 +0200

    Finish Commit

--- src/test/java/homeworkcolcul23/homeworkcolcul23/Service/ColculatorServiceImplTest.java
+++ src/test/java/homeworkcolcul23/homeworkcolcul23/Service/ColculatorServiceImplTest.java
@@ -1,24 +1,44 @@
 package homeworkcolcul23.homeworkcolcul23.Service;
 
+import homeworkcolcul23.homeworkcolcul23.Exception.ZeroDevideException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static homeworkcolcul23.homeworkcolcul23.Service.ColculatorTestConstans.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ColculatorServiceImplTest {
+    private final ColculatorServiceImpl service = new ColculatorServiceImpl();
+
 
     @Test
+
     void sum() {
+        assertEquals(One+Two,service.sum(One,Two));
+        assertEquals(Two+Two,service.sum(Two,Two));
     }
 
     @Test
     void minus() {
+        assertEquals(Three-Two,service.minus(Three,Two));
+        assertEquals(Three-Three,service.minus(Three,Three));
     }
 
     @Test
     void multiply() {
+        assertEquals(Three*Two,service.multiply(Three,Two));
+        assertEquals(Three*One,service.multiply(Three,One));
     }
 
     @Test
     void divide() {
+        assertEquals(Three/One,service.divide(Three,One));
+        assertEquals(Three/Three,service.divide(Three,Three));
+    }
+
+    @Test
+    void SchouldTrowZeroDivide() {
+        assertThrows(ZeroDevideException.class, () -> service.divide(Three, Zero));
     }
 }
---
+++ src/test/java/homeworkcolcul23/homeworkcolcul23/Service/ColculatorTestConstans.java
@@ -0,0 +1,9 @@
+package homeworkcolcul23.homeworkcolcul23.Service;
+
+public class ColculatorTestConstans {
+
+    public static final int Zero = 0;
+    public static final int One = 1;
+    public static final int Two = 2;
+    public static final int Three = 3;
+}
